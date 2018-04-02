49a9e5110e5d3b5d003d19fb660c83fe49d9d288
enscogitans <37674855+enscogitans@users.noreply.github.com>
2018-04-03 01:35:59 +0300

    Исправления в соответствии с замечаниями
    - Код приведён к snake_case
    - Считывание аргументов с помощью argparse
    - Вся реализация спрятана в функции
    - Добавлен класс ConsoleOutAsFile, благодаря которому можно каждый раз не проверять, через что идёт вывод: консоль или файл

--- generate.py
+++ generate.py
@@ -1,115 +1,116 @@
 import re
+import argparse
 import numpy as np
 import random
 from collections import defaultdict
 from fractions import Fraction
 
-# Наша кодировка
-encodeType = 'utf-8'
+encode_type = 'utf-8'  # Наша кодировка
 
 
-# Возвращает пользовательские аргументы
-def get_args():
-    line = input().split()
+# Позволяет использовать те же основные методы, что и
+# открытый через open файл, но только с выводом в консоль
+class ConsoleOutAsFile:
+    def write(self, line):
+        print(line, sep='', end='')
 
-    model_dir = str()
-    seed = ''
-    length = 200
-    output_file = str()
-    is_help = False
-
-    i = 0
-    while i < len(line):
-        if line[i] == '--model':
-            model_dir = line[i + 1]
-            i += 1
-        elif line[i] == '--seed':
-            seed = line[i + 1]
-            i += 1
-        elif line[i] == '--length':
-            length = int(line[i + 1])
-            i += 1
-        elif line[i] == '--output':
-            output_file = line[i + 1]
-            i += 1
-        elif line[i] == '--help':
-            is_help = True
-        i += 1
-
-    return model_dir, seed, length, output_file, is_help
+    def close(self):
+        return
 
 
-# Ввод аргументов
-print('--help, для описания команд')
-modelDirectory, firstWord, length, outputFile, isHelp = get_args()
-while isHelp:
-    if isHelp:
-        print('''Введите:
-            --model <Папка для загрузки модели>
-            (Опционально) --output <Путь до файла, куда будет сохранён текст>,
-                иначе вывод в консоль
-            (Опционально) --seed <Слово>, задаёт первое слово текста
-            (Опционально) --length <Число>, задаёт длину текста, иначе 200
-            (Опционально) --help, для описания команд''')
-    modelDirectory, firstWord, length, outputFile, isHelp = get_args()
+# Создание парсера
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='''Это программа для генерации псевдотекста на основе
+        модели некоторого текста. Эта модель создаётся программой train.py'''
+    )
+    parser.add_argument('-m', '--model', required=True,
+                        help='Путь к файлу, из которого загружается модель'
+                        )
+    parser.add_argument('-s', '--seed',
+                        help='''Начальное слово. Если не указано, оно будет
+                        выбрано случайно'''
+                        )
+    parser.add_argument('-l', '--length', type=int, default=200,
+                        help='Длина генерируемого тектса. По умолчанию - 200'
+                        )
+    parser.add_argument('-o', '--output',
+                        help='''Файл, в который будет записан результат.
+                        Если не указан, результат выведется в консоль.'''
+                        )
+    return parser
 
 
-# Считывание данных из нашей модели
-modelFile = open('{}\\model.txt'.format(modelDirectory),
-                 'r', encoding=encodeType)
+# Считывание данных из нашей модели. Возвращает отдельно слова и вероятности
+def read_model_from_file(model_path):
+    model_file = open('{}\\model.txt'.format(model_path),
+                      'r', encoding=encode_type)
+    model_words = defaultdict(list)
+    model_probabilities = defaultdict(list)
+    for line in model_file:
+        new_line = list(line.split())
+        key = new_line[0]
+        for i in range(1, len(new_line), 2):
+            model_words[key].append(new_line[i])
+            model_probabilities[key].append(Fraction(new_line[i + 1]))
 
-modelWords = defaultdict(list)
-modelProbabilities = defaultdict(list)
-for line in modelFile:
-    newLine = list(line.split())
-    key = newLine[0]
-    for i in range(1, len(newLine), 2):
-        modelWords[key].append(newLine[i])
-        modelProbabilities[key].append(Fraction(newLine[i+1]))
+    return model_words, model_probabilities
 
 
 # Генерация текста и запись его в файл (в stdout)
-result = 0
-if outputFile != '':
-    result = open(outputFile, 'w', encoding=encodeType)
-
-prevWord = firstWord    # Выбор первого слова
-if firstWord == '':
-    prevWord = np.random.choice(modelWords['&'], p=modelProbabilities['&'])
-
-if outputFile != '':    # Печать первого слова
-    result.write(prevWord)
-else:
-    print(prevWord, sep='', end='')
-
-if firstWord != '' and firstWord not in modelWords.keys():
-    # Проверка на корректность первого слова
-    prevWord = '&'
-
-
-for i in range(length - 1):     # Генерация последующих слов
-    nextWord = np.random.choice(modelWords[prevWord],
-                                p=modelProbabilities[prevWord])
-    isSpace = False     # Ставить ли пробел перед очередным словом
-
-    if re.fullmatch(r'[?!.]+', nextWord):
-        # Если точка и тп, пробел не нужен
-        prevWord = '&'
-        isSpace = False
+def generate_and_print_text(model_words, model_probabilities, seed,
+                            length, output):
+    # Открытие файла
+    result_file = 0
+    if output is None:
+        result_file = ConsoleOutAsFile
     else:
-        # Если не запятая и тп, пробел нужен
-        if not re.fullmatch(r'[,;:]+', nextWord):
-            isSpace = True
-        prevWord = nextWord
+        result_file = open(output, 'w', encoding=encode_type)
 
-    if outputFile != '':    # Вывод в файл
-        if isSpace:
-            result.write(' ')
-        result.write(nextWord)
-    else:   # Вывод в консоль
-        if isSpace:
-            print(' ', sep='', end='')
-        print(nextWord, sep='', end='')
+    # Выбор первого слова, и его вывод
+    prev_word = seed
+    if seed is None:
+        prev_word = np.random.choice(model_words['&'])
+    result_file.write(prev_word)
 
-if outputFile != '':
-    result.close()
+    # Проверка этого слова на наличие в нашей модели
+    # Если нет, переобозначим его как конец предложения
+    if seed is not None and prev_word not in model_words.keys():
+        prev_word = '&'
+
+    # Генерация и вывод дальнейших слов
+    for i in range(1, length):
+        next_word = np.random.choice(model_words[prev_word],
+                                     p=model_probabilities[prev_word])
+
+        # Ставить ли пробел перед очередным словом?
+        # "Сдвиг" текущего слова в предыдущее
+        is_space = False
+        if re.fullmatch(r'[?!.]+', next_word):
+            # Если точка и тп, пробел не нужен
+            prev_word = '&'
+            is_space = False
+        else:
+            # Если не запятая и тп, пробел нужен
+            if not re.fullmatch(r'[,;:]+', next_word):
+                is_space = True
+            prev_word = next_word
+
+        # Вывод слова
+        if is_space:
+            result_file.write(' ')
+        result_file.write(next_word)
+
+    result_file.close()
+
+
+# Считывание аргументов
+parser = create_parser()
+args = parser.parse_args()
+
+# Извлечение из модели слов и вероятностей
+model_words, model_probabilities = read_model_from_file(args.model)
+
+# Создание текста и его вывод
+generate_and_print_text(model_words, model_probabilities,
+                        args.seed, args.length, args.output)
