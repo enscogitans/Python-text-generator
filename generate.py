c2b55e9c4f381d2344904aa4dd6ee8740e82444d
enscogitans <37674855+enscogitans@users.noreply.github.com>
2018-04-03 00:15:33 +0300

    Откат. Был закоммичен не тот файл

--- generate.py
+++ generate.py
@@ -1,123 +1,115 @@
 import re
-import argparse
-from fractions import Fraction
+import numpy as np
+import random
 from collections import defaultdict
+from fractions import Fraction
 
-# Наш алфавит и используемая кодировка
-alphabet = re.compile(u'[ёЁа-яА-Я0-9]+[-]?[ёЁа-яА-Я0-9]+|[.,?!;:]+')
-encode_type = 'utf-8'
-
-
-# Создание парсера
-def create_parser():
-    parser = argparse.ArgumentParser(
-        description='''Это программа для создания модели введённого текста.
-        Созданная модель в дальнейшем используется для генерации псевдотекста
-        программой generate.py'''
-    )
-    parser.add_argument('-id', '--input-dir',
-                        help='''Директория, в которой лежит коллекция документов.
-                        Если не указано, ввод текта осуществляется из консоли.
-                        В данном случае необходимо закончить ввод данных вводом
-                        строки: '////' '''
-                        )
-    parser.add_argument('-m', '--model', required=True,
-                        help='Путь к файлу, в который сохраняется модель')
-    parser.add_argument('-lc', '--lowercase', type=bool, default=False,
-                        help='Приводить тексты к lowercase')
-    return parser
+# Наша кодировка
+encodeType = 'utf-8'
 
 
-# Построчно генерирует строки из данного файла (stdin)
-def line_generator(file_path):
-    if file_path is not None:
-        file = open(file_path, 'r', encoding=encode_type)
-        for line in file:
-            yield str(bytes(line, encode_type).decode(encode_type))
+# Возвращает пользовательские аргументы
+def get_args():
+    line = input().split()
+
+    model_dir = str()
+    seed = ''
+    length = 200
+    output_file = str()
+    is_help = False
+
+    i = 0
+    while i < len(line):
+        if line[i] == '--model':
+            model_dir = line[i + 1]
+            i += 1
+        elif line[i] == '--seed':
+            seed = line[i + 1]
+            i += 1
+        elif line[i] == '--length':
+            length = int(line[i + 1])
+            i += 1
+        elif line[i] == '--output':
+            output_file = line[i + 1]
+            i += 1
+        elif line[i] == '--help':
+            is_help = True
+        i += 1
+
+    return model_dir, seed, length, output_file, is_help
+
+
+# Ввод аргументов
+print('--help, для описания команд')
+modelDirectory, firstWord, length, outputFile, isHelp = get_args()
+while isHelp:
+    if isHelp:
+        print('''Введите:
+            --model <Папка для загрузки модели>
+            (Опционально) --output <Путь до файла, куда будет сохранён текст>,
+                иначе вывод в консоль
+            (Опционально) --seed <Слово>, задаёт первое слово текста
+            (Опционально) --length <Число>, задаёт длину текста, иначе 200
+            (Опционально) --help, для описания команд''')
+    modelDirectory, firstWord, length, outputFile, isHelp = get_args()
+
+
+# Считывание данных из нашей модели
+modelFile = open('{}\\model.txt'.format(modelDirectory),
+                 'r', encoding=encodeType)
+
+modelWords = defaultdict(list)
+modelProbabilities = defaultdict(list)
+for line in modelFile:
+    newLine = list(line.split())
+    key = newLine[0]
+    for i in range(1, len(newLine), 2):
+        modelWords[key].append(newLine[i])
+        modelProbabilities[key].append(Fraction(newLine[i+1]))
+
+
+# Генерация текста и запись его в файл (в stdout)
+result = 0
+if outputFile != '':
+    result = open(outputFile, 'w', encoding=encodeType)
+
+prevWord = firstWord    # Выбор первого слова
+if firstWord == '':
+    prevWord = np.random.choice(modelWords['&'], p=modelProbabilities['&'])
+
+if outputFile != '':    # Печать первого слова
+    result.write(prevWord)
+else:
+    print(prevWord, sep='', end='')
+
+if firstWord != '' and firstWord not in modelWords.keys():
+    # Проверка на корректность первого слова
+    prevWord = '&'
+
+
+for i in range(length - 1):     # Генерация последующих слов
+    nextWord = np.random.choice(modelWords[prevWord],
+                                p=modelProbabilities[prevWord])
+    isSpace = False     # Ставить ли пробел перед очередным словом
+
+    if re.fullmatch(r'[?!.]+', nextWord):
+        # Если точка и тп, пробел не нужен
+        prevWord = '&'
+        isSpace = False
     else:
-        while True:
-            line = input()
-            if line == '////':
-                return
-            yield line
+        # Если не запятая и тп, пробел нужен
+        if not re.fullmatch(r'[,;:]+', nextWord):
+            isSpace = True
+        prevWord = nextWord
 
+    if outputFile != '':    # Вывод в файл
+        if isSpace:
+            result.write(' ')
+        result.write(nextWord)
+    else:   # Вывод в консоль
+        if isSpace:
+            print(' ', sep='', end='')
+        print(nextWord, sep='', end='')
 
-# Обрабатывает строки, возвращая отдельные слова, согласованные
-# с алфавитом, и, при необходимости, в нижнем регистре
-def token_generator(lines, is_lower):
-    for line in lines:
-        for token in alphabet.findall(line):
-            yield token.lower() if is_lower else token
-
-
-# По словам (токенам) возвращает биграммы
-def bigram_generator(tokens):
-    t0 = '&'
-    for t1 in tokens:
-        yield t0, t1
-        if re.fullmatch(r'[?!.]+', t1):
-            t0 = '&'
-        else:
-            t0 = t1
-
-
-# Подсчёт количества слов и пар слов из биграмм соответственно
-def count_words_and_pairs(bigrams):
-    word_freq = defaultdict(lambda: 0)
-    pair_freq = defaultdict(lambda: 0)
-
-    last_symbol = '&'
-    word_freq['&'] = 1
-    for (t0, t1) in bigrams:
-        if re.fullmatch(r'[?!.]+', t1):
-            word_freq['&'] += 1
-        word_freq[t1] += 1
-        pair_freq[(t0, t1)] += 1
-        last_symbol = t1
-
-    if re.fullmatch(r'[?!.]+', last_symbol):
-        pair_freq[('&', '&')] += 1
-    else:
-        pair_freq[(last_symbol, '&')] += 1
-    return word_freq, pair_freq
-
-
-# Инициализация нашей модели. По ключу хранится лист слов,
-# которые могут идти после него с указаним вероятности (в виде рац. дроби)
-def initialise_model(word_freq, pair_freq):
-    model = dict()
-    for (t0, t1), freq in pairFreq.items():
-        if t0 in model:
-            model[t0].append((t1, Fraction(freq, wordFreq[t0])))
-        else:
-            model[t0] = [(t1, Fraction(freq, wordFreq[t0]))]
-    return model
-
-
-# Сохранение модели в файл model.txt в указанной папке
-def save_model(model, model_path):
-    model_file = open('{}\\model.txt'.format(model_path), 'w',
-                      encoding=encode_type)
-    for key, lst in model.items():
-        model_file.write(key)
-        for (word, freq) in lst:
-            model_file.write(' {} {}'.format(word, freq))
-        model_file.write('\n')
-    model_file.close()
-
-
-# Считывание аргументов
-parser = create_parser()
-args = parser.parse_args()
-
-# Инициализация генератора биграмм
-lines = line_generator(args.input_dir)
-tokens = token_generator(lines, args.lowercase)
-bigrams = bigram_generator(tokens)
-
-# Подсчёт количества слов и пар слов соответственно
-word_freq, pair_freq = count_words_and_pairs(bigrams)
-
-# Инициализация и сохранение нашей модели
-model = initialise_model(word_freq, pair_freq)
-save_model(model, args.model)
+if outputFile != '':
+    result.close()
